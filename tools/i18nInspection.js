fae330cb9ee509f7a8bf0801fc016841ecae187b
nzhming <1078830088@qq.com>
2022-08-31 21:01:58 +0800

    inspection；

--- tools/i18nInspection.js
+++ tools/i18nInspection.js
@@ -1,9 +1,20 @@
 const path = require('path')
-const { directoryReader, extractFiles } = require('./directoryReader');
+const {
+  directoryReader,
+  extractFiles,
+  flatKeysList
+} = require('./directoryReader');
 
 const langPath = 'D:\\Projects\\UMC\\umc-web\\src\\lang';
 
-const excludeDirectory = ['node_modules', 'elementLang'];
+const excludeDirectory = [
+  'node_modules',
+  'elementLang',
+  'cm',
+  'auditManage',
+  'monitoringCenter',
+  'shortChain'
+];
 const includeFile = ['js'];
 const langContent = directoryReader(
   langPath,
@@ -20,9 +31,6 @@ for(const jsFile of jsFiles) {
   if (!transFiles[directoryName]) {
     transFiles[directoryName] = {};
   }
-  if (fileName.includes('utpl')) {
-    continue;
-  }
   transFiles[directoryName][fileName] = filePath;
 }
 
@@ -35,9 +43,56 @@ for(const [, files] of Object.entries(transFiles)) {
   const en = require(files['en.js']);
   Object.assign(cnData, cn);
   Object.assign(twData, tw);
-  Object.assign(enData, en)
+  Object.assign(enData, en);
 }
 
-const json = JSON.stringify(cnData, null, 2);
+const moduleLevel = [
+  [],
+  ['common'],
+  ['aimEdit'],
+  ['contact'],
+  ['fgEdit'],
+  ['fgTemplate'],
+  ['headerIcon'],
+  ['loginPage'],
+  ['rmsEdit'],
+  ['statistics'],
+  ['templateManage'],
+  ['utplSend'],
+  ['utpltemplate']
+];
+let langList = flatKeysList(cnData, moduleLevel);
+for(const item of langList) {
+  const { i18nKey } = item;
+  const splitor = '.';
+  let moduleName;
+  if (i18nKey.includes(splitor)) {
+    moduleName = i18nKey.split(splitor)[0];
+  } else {
+    moduleName = 'public';
+  }
+  item['moduleName'] = moduleName;
+  item['twValue'] = eval(`twData.${i18nKey}`);
+  item['enValue'] = eval(`enData.${i18nKey}`);
+}
+console.log(langList.length);
+
+
+function jsonToJS(json) {
+  json = JSON.stringify(json, null, 2);
+  json = json
+    .replace(/"\S*":/g, key => key.replace(/"/g, ''))
+    .replace(/'/g, "\\'")
+    .replace(/&nbsp;/g, "")
+    .replace(/"/g, "'");
+  return `module.exports = ${json}\n`;
+}
+
+// const json = JSON.stringify(cnData, null, 2);
 const { writeFileSync: writer } = require('fs');
-writer(`../output/${path.basename(langPath)}.json`, json);
+for(const row of langList) {
+
+}
+writer(`../output/${path.basename(langPath)}_list.js`, jsonToJS(langList));
+// writer(`../output/${path.basename(langPath)}_tw.js`, jsonToJS(twData));
+// writer(`../output/${path.basename(langPath)}_en.js`, jsonToJS(enData));
