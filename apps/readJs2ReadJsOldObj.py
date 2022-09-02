70e51257fe12b9e4521ff3385e8fec5e2a3c7d30
nzhming <1078830088@qq.com>
2022-09-02 10:13:45 +0800

    readJs2ReadJsOldObj.py

--- apps/readJs2ReadJsOldObj.py
+++ apps/readJs2ReadJsOldObj.py
@@ -18,7 +18,7 @@ def get_filelist(dir,filetype):
     for home, dirs, files in os.walk(dir):
         for filename in files:
             if(filename[-len(filetype):]==filetype):
-                Filelist.append(os.path.join(home, filename))    
+                Filelist.append(os.path.join(home, filename))
     if Filelist==[]:
         Filelist=[dir]
     return Filelist
@@ -30,7 +30,7 @@ def getObjUrl(moduleName,dir):
     file_type=[".vue"]
     for type in file_type:
         print("开始处理"+type+"类型文件")
-        file_list = get_filelist(dir,type) 
+        file_list = get_filelist(dir,type)
         for file in file_list:
             with io.open(file, "r", encoding="utf-8") as f:
                 t=f.read()
@@ -43,7 +43,7 @@ def getObjUrl(moduleName,dir):
     return fileList
 
 #Lang：获取Lang语言对象
-def getDictObj(t): 
+def getDictObj(t):
     #处理格式，转为字典
     t=t.replace('module.exports = ','')
     dictObj=demjson.decode(t)
@@ -54,7 +54,7 @@ def getListObj(obj):
     list=[]
     for key,val in obj.items():
         item=getKeyVal(key,val)
-        list.extend(item) 
+        list.extend(item)
     # print("结果 list:",list)
     return list
 
@@ -62,10 +62,10 @@ def getListObj(obj):
 def getKeyVal(key,val):
     list=[]
     if type(val)==str:
-        item=[key,val] 
+        item=[key,val]
         list.append(item)
         return list
-    elif type(val)==dict: 
+    elif type(val)==dict:
         for key2,val2 in val.items():
             item=getKeyVal(key+"."+key2,val2)
             list.extend(item)
@@ -73,10 +73,10 @@ def getKeyVal(key,val):
 
 #Lang：获取Lang语言对象
 def getTempList(file_name):
-    with io.open(file_name, "r", encoding="utf-8") as f: 
+    with io.open(file_name, "r", encoding="utf-8") as f:
         t=f.read()
         dictObj=getDictObj(t)
-        list=getListObj(dictObj) 
+        list=getListObj(dictObj)
         # for objItem in list:
         #     print("zh：【"+objItem[1]+"】   key：【"+objItem[0]+"】")
         return list
@@ -89,8 +89,8 @@ def replace_JS(obj,dir):
     look_up_table=obj
     for type in file_type:
         print("开始处理"+type+"类型文件")
-        file_list = get_filelist(dir,type) 
-        for file in file_list: 
+        file_list = get_filelist(dir,type)
+        for file in file_list:
             file_data=""
             with io.open(file, "r", encoding="utf-8") as f:
                 t=f.read()
@@ -108,9 +108,9 @@ def replaceJSfile(str,zh,key):
         ["'"+zh+"'", "i18n.tc('"+key+"')"],#'姓名'
     ]
     for objItem in obj:
-        str=str.replace(objItem[0],objItem[1]) 
+        str=str.replace(objItem[0],objItem[1])
         print("replaceJSfile oldStr：【"+objItem[0]+"】   newStr：【"+objItem[1]+"】")
-    
+
     importStr='import { i18n } from "@/main"'
     if str.find('i18n')!=-1&str.find(importStr)==-1:
         if(str.find('<script>')!=-1):
@@ -135,7 +135,7 @@ def replace_Vue(obj,dir):
                 file_data=replace_VueText(t,look_up_table)
             with io.open(file,"w",encoding="utf-8") as f:
                 f.write(file_data)
-    print("结束处理"+type+"类型文件") 
+    print("结束处理"+type+"类型文件")
 
 #2.0
 def replace_VueText(str,look_up_table):
@@ -146,26 +146,26 @@ def replace_VueText(str,look_up_table):
     vueJsStrOld=vueJsStr
 
     for item in look_up_table:
-        #替换HTML部分       
+        #替换HTML部分
         htmlStr=replaceHTML(htmlStr,item[1],item[0])
         #替换Script部分
-        vueJsStr=replaceVUEJS(vueJsStr,item[1],item[0]) 
+        vueJsStr=replaceVUEJS(vueJsStr,item[1],item[0])
         if item[1].find('！'):
             htmlStr=replaceHTML(htmlStr,item[1].replace('！','!'),item[0])
             vueJsStr=replaceVUEJS(vueJsStr,item[1].replace('！','!'),item[0])
     str=str.replace(htmlStrOld,htmlStr)
-    str=str.replace(vueJsStrOld,vueJsStr) 
+    str=str.replace(vueJsStrOld,vueJsStr)
     return str
 
 #2.1 HMl
 def replaceHTML(str,zh,key):
     if key.find('{0}')==-1:
         #格式纠正
-        str=replaceFormatPer1(str,zh,key) 
+        str=replaceFormatPer1(str,zh,key)
 
         #html 日期相关 Label替换标签相关
-        str=replaceFormat_htmlLabel_Date(str,zh,key)    
-        #htmlLabel替换标签相关    
+        str=replaceFormat_htmlLabel_Date(str,zh,key)
+        #htmlLabel替换标签相关
         str=replaceFormat_htmlLabel(str,zh,key)
 
         #htmlText
@@ -181,31 +181,31 @@ def replaceVUEJS(str,zh,key):
         #script
         str=replaceFormat_VUEScript(str,zh,key)
     return str
- 
+
 #格式纠正
-def replaceFormatPer1(str,zh,key): 
+def replaceFormatPer1(str,zh,key):
     obj=[
         ['： "','："'],
         [': "',':"'],
     ]
     for objItem in obj:
-        str=str.replace(objItem[0],objItem[1]) 
+        str=str.replace(objItem[0],objItem[1])
         print("replaceFormatPer1 oldStr：【"+objItem[0]+"】   newStr：【"+objItem[1]+"】")
     return str
 
 #html 日期相关 Label替换标签相关
 def replaceFormat_htmlLabel_Date(str,zh,key):
     #日期组件可能会存在的情况
-    dateLabelList=['至','开始日期','开始时间','结束日期','结束时间'] 
+    dateLabelList=['至','开始日期','开始时间','结束日期','结束时间']
     #少数日期组件替换
     for dateItem in dateLabelList:
         #当符合列表内容时才会替换
         if dateItem==zh:
-            attr=[ 
+            attr=[
                 'range-separator',
                 'start-placeholder',
-                'end-placeholder', 
-            ]    
+                'end-placeholder',
+            ]
             for attrItem in attr:
                 obj=[
                     [attrItem+'="'+zh+'"' , ':'+attrItem+'="$t('+"'"+key+"'"+')"'],
@@ -213,7 +213,7 @@ def replaceFormat_htmlLabel_Date(str,zh,key):
                     # [attrItem+'="'+zh+':"' , ':'+attrItem+'="$t('+"'"+key+"'"+')+`:`"'],
                 ]
                 for objItem in obj:
-                    str=str.replace(objItem[0],objItem[1]) 
+                    str=str.replace(objItem[0],objItem[1])
                     print("replaceFormat_htmlLabel_Date oldStr：【"+objItem[0]+"】   newStr：【"+objItem[1]+"】")
     return str
 
@@ -224,7 +224,7 @@ def replaceFormat_htmlLabel(str,zh,key):
         'title',
         'element-loading-text',
         'placeholder'
-    ]    
+    ]
     for attrItem in attr:
         obj=[
             [attrItem+'="'+zh+'"' , ':'+attrItem+'="$t('+"'"+key+"'"+')"'],
@@ -232,11 +232,11 @@ def replaceFormat_htmlLabel(str,zh,key):
             [attrItem+'="'+zh+':"' , ':'+attrItem+'="$t('+"'"+key+"'"+')+`:`"'],
         ]
         for objItem in obj:
-            str=str.replace(objItem[0],objItem[1]) 
+            str=str.replace(objItem[0],objItem[1])
             print("replaceFormat_htmlLabel oldStr：【"+objItem[0]+"】   newStr：【"+objItem[1]+"】")
     return str
 
-#htmlText  
+#htmlText
 def replaceFormat_htmlText(str,zh,key):
     obj=[
         ['>'+zh+'<', ">{{ $t('"+key+"') }}<"],#<span>确定</span>
@@ -245,43 +245,43 @@ def replaceFormat_htmlText(str,zh,key):
     ]
 
     for objItem in obj:
-        str=str.replace(objItem[0],objItem[1]) 
+        str=str.replace(objItem[0],objItem[1])
         print("replaceFormat_htmlText oldStr：【"+objItem[0]+"】   newStr：【"+objItem[1]+"】")
-    return str 
-    
-#htmlScript 
+    return str
+
+#htmlScript
 def replaceFormat_htmlScript(str,zh,key):
     obj=[
         ["`"+zh+"`", "$t('"+key+"')"], #: '确定'
         ["'"+zh+"：'", "$t('"+key+"')+`：`"],#'确定：'
         ['"'+zh+'："', "$t('"+key+"')+`：`"],#"确定："
         ["'"+zh+":'", "$t('"+key+"')+`:`"],#'确定:'
-        ['"'+zh+':"', "$t('"+key+"')+`:`"],#"确定:" 
+        ['"'+zh+':"', "$t('"+key+"')+`:`"],#"确定:"
         ["'"+zh+"'", "$t('"+key+"')"], #'确定'
         # ['"'+zh+'"', "$t('"+key+"')"], #"确定"
     ]
 
     for objItem in obj:
-        str=str.replace(objItem[0],objItem[1]) 
+        str=str.replace(objItem[0],objItem[1])
         print("replaceFormat_htmlScript oldStr：【"+objItem[0]+"】   newStr：【"+objItem[1]+"】")
     return str
 
-#VUEScript 
+#VUEScript
 def replaceFormat_VUEScript(str,zh,key):
-    obj=[ 
+    obj=[
         ["`"+zh+"`", "i18n.tc('"+key+"')"], #: `确定`
         ["'"+zh+"：'", "i18n.tc('"+key+"')+`：`"],#'确定：'
         ["'"+zh+":'", "i18n.tc('"+key+"')+`:`"],#'确定：'
         ['"'+zh+'："', "i18n.tc('"+key+"')+`：`"],#"确定："
-        ['"'+zh+':"', "i18n.tc('"+key+"')+`:`"],#"确定:" 
+        ['"'+zh+':"', "i18n.tc('"+key+"')+`:`"],#"确定:"
         ["'"+zh+"'", "i18n.tc('"+key+"')"], #'确定'
         ['"'+zh+'"', "i18n.tc('"+key+"')"], #"确定"
     ]
 
     for objItem in obj:
-        str=str.replace(objItem[0],objItem[1]) 
+        str=str.replace(objItem[0],objItem[1])
         print("replaceFormat3 oldStr：【"+objItem[0]+"】   newStr：【"+objItem[1]+"】")
-    
+
     importStr='import { i18n } from "@/main"'
     if str.find('i18n')!=-1&str.find(importStr)==-1:
         if(str.find('<script>')!=-1):
@@ -289,12 +289,26 @@ def replaceFormat_VUEScript(str,zh,key):
             importStr='<script>'+'\n'+importStr
         str=importStr+'\n'+str
     return str
- 
+
 #1 替换数据
 def replaceData(obj,dir):
     replace_Vue(obj,dir)
-    replace_JS(obj,dir) 
+    replace_JS(obj,dir)
 
+
+# ReadOldObj:自动添加 import { mapState } from 'vuex'
+def autoStyleIsFlag():
+    dirList=getReplaceDirList()
+    for item in dirList:
+        if item[1]:
+            file=item[2]
+            with io.open(file, "r", encoding="utf-8") as f:
+                str=f.read()
+                importStr="import { mapState } from 'vuex'"
+                if(str.find('<script>')!=-1):
+                    str=str.replace('<script>'+'\n','<script>'+'\n'+importStr+'\n')
+            with io.open(file,"w",encoding="utf-8") as f:
+                f.write(str)
 
 # ReadOldObj:获取新的项目 需要做多语言的 地址
 def getNewReplaceDirList():
@@ -308,8 +322,8 @@ def getNewReplaceDirList():
 def getReplaceDirList():
     oldObjDir='D:\\UMCgit\\guojihua\\umc-web\\'# 旧的项目文件夹
     newObjDir='D:\\UMCgit\\312\\umc-web\\'# 新的项目文件夹
-    dir='src\\components\\BaseChart'# 子文件夹 dir=''
-    moduleName='statistics.' # 指定模块【会查询是否有'$t(\''+moduleName 或 'tc(\''+moduleName】 moduleName=''    
+    dir='src\\components'# 子文件夹 dir=''
+    moduleName='statistics.' # 指定模块【会查询是否有'$t(\''+moduleName 或 'tc(\''+moduleName】 moduleName=''
     dirList=getObjUrl(moduleName,oldObjDir+dir)# 获取旧的项目 做过相关多语言的 地址
     for item in dirList:
         item.append(item[0].replace(oldObjDir,newObjDir))
@@ -325,20 +339,21 @@ def replaceText():
     #     "D:\\UMCgit\\312\\umc-web\\src\\views\\statistics\\form",
     #     "D:\\UMCgit\\312\\umc-web\\src\\views\\statistics\\analysis",
     #     "D:\\UMCgit\\312\\umc-web\\src\\views\\fg\\statistics",
-    # ] 
+    # ]
 
     # 通过读取旧的项目，获取要替换的文件夹及文件
     newReplaceDirList=getNewReplaceDirList()# 获取新的项目 需要做多语言的 地址
     # print('newReplaceDirList:',newReplaceDirList)
-
     for dirItem in newReplaceDirList:
         for langItem in langFileList:
             moduleKey=getTempList(langItem)
             replaceData(moduleKey,dirItem) # 替换
 
+    autoStyleIsFlag()#自动添加 import { mapState } from 'vuex'
+
 
 #程序入口
-if __name__ =="__main__": 
+if __name__ =="__main__":
     replaceText()
 
 
