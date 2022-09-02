d294f8289cc011fc558cce507cfb5124c300d0f3
nzhming <1078830088@qq.com>
2022-09-02 14:33:56 +0800

    新增中文检索功能：chineseInspector

---
+++ apps/chineseInspector.js
@@ -0,0 +1,97 @@
+const {
+  directoryReader,
+  extractFiles
+} = require('../tools/directoryReader.js');
+
+const {
+  readFileSync: reader,
+  writeFileSync: writer
+} = require('fs');
+
+const { logger } = require('../logger');
+
+const path = require('path');
+
+const { replaceComments } = require('../tools/i18nAutoReplacement');
+
+(async () => {
+  // 检索中文的文件夹
+  const directoryPath = 'D:\\Projects\\UMC\\dev\\umc-web\\src\\views\\fg\\template';
+  const excludeDirectory = ['node_modules', 'backend-emp', 'frontend-emp', 'security-emp'];
+  const excludeFile = ['ChannelNumItem-emp.vue'];
+  const includeExt = ['vue'];
+
+  // 额外的文件
+  const additionalFileList = [
+    // 'D:\\Projects\\UMC\\dev\\umc-web\\src\\components\\Collapse.vue',
+    // 'D:\\Projects\\UMC\\dev\\umc-web\\src\\components\\PageLimitsItem.vue'
+  ];
+
+  // 是否忽略注释里的内容；
+  const excludeComments = true;
+
+  // 单次任务检索的文件数
+  const taskBlockSize = 10;
+
+  logger(`##### Start chinese inspector #####`);
+
+  logger(`##### Reading directory #####`);
+  // 读取目标文件夹中所有文件
+  const directoryContent = directoryReader(
+    directoryPath,
+    {
+      excludeDirectory,
+      excludeFile,
+      includeExt
+    }
+  );
+  const files = extractFiles(directoryContent, additionalFileList);
+
+  // 开始
+  const taskBlockQueue = [];
+  const fileList = [];
+
+  for(const [fileType, list] of Object.entries(files)) {
+    fileList.push(...list);
+    logger(`${fileType} file:${list.length}`);
+  }
+  const totalFileCount = fileList.length;
+  logger(`Total file:${totalFileCount}`);
+  let currentFileCount = 0;
+
+  let chineseList = [];
+
+  // 处理js文件
+  while(fileList.length) {
+    taskBlockQueue.push([...fileList.splice(0, taskBlockSize)]);
+  }
+  while(taskBlockQueue.length) {
+    const files = taskBlockQueue.shift();
+    // 读文件
+    for(const file of files) {
+      currentFileCount++;
+      logger(`Current file:${path.basename(file.filePath)}(${currentFileCount}/${totalFileCount})`)
+      let fileText;
+      try {
+        fileText = reader(file.filePath, 'utf8');
+      } catch (e) {
+        logger(`Error while reading File \`${file.filePath}\``);
+        console.trace(e);
+        continue;
+      }
+
+      if (excludeComments) {
+        fileText = replaceComments(fileText);
+      }
+      const chineseReg = /[\u4e00-\u9fa5，（）。？！、；：]+/g;
+      let matches = fileText.match(chineseReg) || [];
+      chineseList.push(...matches);
+    }
+  }
+
+  chineseList = [...new Set(chineseList)];
+  logger(`Total found chinese:${chineseList.length}`);
+  writer('../output/chinese.json', JSON.stringify(chineseList, null, 2));
+  logger(`Find \`chinese.json\` in \`output\``);
+  logger(`##### Finish chinese inspector #####`);
+})();
