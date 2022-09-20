23f3deeb3a9a99fb35ae391dab58143e96df8b73
nzhming <1078830088@qq.com>
2022-09-20 11:20:25 +0800

    add init script

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('I18n Tools');
+const { access, mkdir } = require('fs');
+
+const { exec } = require('child_process');
+
+const neededDirs = ['input', 'output', 'logger'];
+neededDirs.forEach(dirName => {
+  const dirPath = `./${dirName}`;
+  access(
+    dirPath,
+    err => {
+      if (err) {
+        mkdir(
+          dirPath,
+          err => {
+            if (err) {
+              console.trace(err);
+            }
+          }
+        );
+      }
+    }
+  );
+  console.log(`The \`${dirName}\` directory is available!`);
+});
+
+console.log('Running `npm install`');
+exec(
+  'npm install',
+  (err, stdout) => {
+    if (err) {
+      console.trace(err);
+      return;
+    }
+
+    console.log('> npm install');
+    console.log(stdout);
+  }
+);
